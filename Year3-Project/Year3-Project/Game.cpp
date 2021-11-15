97683b32c281d970035b218563f63072ea28a87c
unknown <c00248444@itcarlow.ie>
2021-11-15 15:37:44 +0000

    setup vector of objects for the video

--- Year3-Project/Year3-Project/Game.cpp
+++ Year3-Project/Year3-Project/Game.cpp
@@ -24,7 +24,7 @@ Game::Game() :
     m_grid = Grid(screen_Height / tileSize, screen_Width / tileSize);
     m_worldView.setCenter(m_spawnPosition);
     pauseMenuSetUp();
-    m_environment = Environment(74, screen_Height / tileSize, screen_Width / tileSize, 0);
+    setupEnvironment();
 
     m_worldView.reset(sf::FloatRect(m_player.getPosition().x, m_player.getPosition().y, screen_Width / 2, screen_Height / 2));
 }
@@ -172,7 +172,10 @@ void Game::render()
             m_window.clear(sf::Color::Color(52, 168, 235));
             //m_window.draw(m_gameMenu);
             m_window.setView(m_worldView);
-            m_environment.render(m_window);
+            for (Environment env : m_environment)
+            {
+                env.render(m_window);
+            }
             m_window.draw(m_player);
             m_window.draw(m_pickup);
             m_window.draw(m_enemy);
@@ -187,7 +190,10 @@ void Game::render()
             break;
         case GameState::PAUSE:
             m_window.setView(m_worldView);
-            m_environment.render(m_window);
+            for (Environment env : m_environment)
+            {
+                env.render(m_window);
+            }
             m_window.draw(m_player);
             m_window.draw(m_pickup);
             m_window.draw(m_enemy);
@@ -207,7 +213,10 @@ void Game::checkCollisions()
 {
     collisions.check(m_player, m_enemy);
     collisions.check(m_player, m_pickup);
-    collisions.check(m_player, m_environment);
+    for (Environment env : m_environment)
+    {
+        collisions.check(m_player, env);
+    }
 }
 
 void Game::pauseMenuSetUp()
@@ -215,6 +224,16 @@ void Game::pauseMenuSetUp()
     m_pauseRect.setSize(sf::Vector2f(screen_Width/4, screen_Height/4));
     m_pauseRect.setFillColor(sf::Color::Color(225, 0, 0, 100));
     m_pauseRect.setOrigin(screen_Width/8,screen_Height/8);
+}
+
+void Game::setupEnvironment()
+{
+    //temp manually setting it
+    int nums[] = { 73, 74, 75, 77, 78, 79, 43, 44, 45, 12 };
+    for (int i = 0; i < 10; i++)
+    {
+        m_environment.push_back(Environment(nums[i], screen_Height / tileSize, screen_Width / tileSize, 0));
+    }
 }
 
 void Game::cameraMovement(sf::Time dt)
